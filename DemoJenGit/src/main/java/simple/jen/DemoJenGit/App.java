25b15a5f94485359b2e2a800059f1a57f8d32855
razzaksr <razzaksr@gmail.com>
2019-10-16 01:05:09 +0530

    project added

---
+++ DemoJenGit/src/main/java/simple/jen/DemoJenGit/App.java
@@ -0,0 +1,13 @@
+package simple.jen.DemoJenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldin Zealous!" );
+    }
+}
